06f960322954fabc87542b48a81751e60dda89c0
angelskydiver <158997505+angelskydiver@users.noreply.github.com>
2024-11-16 11:19:23 +0000

    feat: expand MicroMart application modules

---
+++ job-service/src/main/java/com/safalifter/jobservice/model/Job.java
@@ -0,0 +1,31 @@
+package com.safalifter.jobservice.model;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.util.Collections;
+import java.util.List;
+
+@Entity(name = "jobs")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Job extends BaseEntity {
+    private String name;
+    private String description;
+    private String imageId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "job", cascade = CascadeType.ALL)
+    private List<Advert> adverts;
+
+    @ElementCollection
+    @CollectionTable(name = "jobs_keys", joinColumns = @JoinColumn(name = "job_id"))
+    @Column(name = "key_value")
+    private List<String> keys = Collections.emptyList();
+}
