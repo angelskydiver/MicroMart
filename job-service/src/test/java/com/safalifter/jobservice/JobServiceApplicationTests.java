56fce6e048f0fddc9c6704993e573a8c086aa515
angelskydiver <158997505+angelskydiver@users.noreply.github.com>
2025-09-29 08:19:00 +0000

    feat: expand MicroMart application modules

---
+++ job-service/src/test/java/com/safalifter/jobservice/JobServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.safalifter.jobservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class JobServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
